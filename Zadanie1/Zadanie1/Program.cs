d23a992a74a4c1ea589557f75581bf30b3f6b182
Nikitina000 <148875590+Nikitina000@users.noreply.github.com>
2023-10-28 13:08:03 +0500

    Add files via upload

---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,49 @@
+﻿using MySqlConnector;
+
+namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Eat> eats = new List<Eat>();
+            string connecting = "server=localhost;user id=voloda;password=rhenj;database=sys"; ;
+            
+
+            using (var conn = new MySqlConnection(connecting))
+            {
+                conn.Open();
+                using (var command = conn.CreateCommand())
+                {
+                   
+                    command.CommandText = "SELECT * FROM eat WHERE weight > @weight";
+                    int weight = 400;
+                    command.Parameters.AddWithValue("@weight",weight);
+                    using (var reader = command.ExecuteReader())
+                    {
+                       
+                        while (reader.Read())
+                        {
+                            eats.Add(new Eat
+                            {
+                                Id = reader.GetInt32(0),
+                                name = reader.GetString(1),
+                                weight = reader.GetInt32(2)
+                            });
+                        }
+                    }
+                }
+                    
+             
+            }
+            Console.WriteLine($"ID Name Weight");
+            foreach (var a in eats)
+            {
+                Console.WriteLine($"{a.Id} {a.name} {a.weight}");
+            }
+
+
+
+        }
+    }
+}
